6569e1da373258b8942b0a79956e44537128f3e5
noobie programmer <56153261+Drownedre@users.noreply.github.com>
2023-06-06 06:41:58 +0700

    Update input_siswa.php
    a little update for new api

--- PDO/view/input_siswa.php
+++ PDO/view/input_siswa.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <form method="post" action="../controller/proses_input.php">
+        NIS : <input type="number" name="nis"> <br>
+        NAMA : <input type="text" name="nama"> <br>
+        TANGGAL LAHIR : <input type="date" name="tanggal"> <br>
+        <input type="SUBMIT" value="INPUT">
+    </form>
 </body>
+
 </html>
