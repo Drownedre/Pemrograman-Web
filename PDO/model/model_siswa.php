480b7b5e2376d34063e67e70ae70ee6b3e326d3b
noobie programmer <56153261+Drownedre@users.noreply.github.com>
2023-06-06 06:40:35 +0700

    Update model_siswa.php
    a little change due to new api

--- PDO/model/model_siswa.php
+++ PDO/model/model_siswa.php
@@ -1,46 +1,83 @@
 
 <?php
-    class mydb {
-//Connect
-        public $host = "localhost";
-        public $dbname = "db_sisw4";
-        public $username = "root";
-        public $password = "";
-        public $db;
+class mydb
+{
+    //atribut koneksi database
+    public $host = "localhost";
+    public $dbname = "db_siswa";
+    public $username = "root";
+    public $password = "";
+    public $db;
 
-//Script Connect
-        public function __construct()
-        {
-            $this->db= new PDO(
-                "mysql:host={$this->host};
-                dbname={$this->dbname}",
-                $this->username,
-                $this->password
-            );
-        }
+    //koneksi
+    public function __construct()
+    {
+        $this->db = new PDO(
+            "mysql:host={$this->host};
+            dbname={$this->dbname}",
+            $this->username,
+            $this->password
+        );
+    }
 
-        public function tampilkan() {
-            $sql = "SELECT * from tb_siswa";    
-            $query = $this->db->prepare($sql);
-            $query->execute();
-            $data = $query->fetchAll();
-            return $data;
-        }
-        
-        public function insert($a, $b, $c) {
-            $sql = "INSERT INTO tb_siswa(nis, nama, tanggal_lahir) values (:nis, :nama, :tanggal_lahir)";
-            
-            $query = $this->db->prepare($sql);
+    public function tampilkan()
+    {
+        $sql = "SELECT * from tb_siswa";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+        $data = $query->fetchAll();
+        return $data;
+    }
 
-            $query->bindParam(":nis", $a);
-            $query->bindParam(":nama", $b);
-            $query->bindParam(":tanggal_lahir", $c);
+    public function insert($a, $b, $c)
+    {
+        $sql = "INSERT INTO tb_siswa(nis,nama,tanggal_lahir) values (:nis, :nama, :tanggal_lahir)";
 
-            if($query->execute()) {
-                return true;
-            } else {
-                return false;
-            }
+        $query = $this->db->prepare($sql);
+
+        $query->bindParam(":nis", $a);
+        $query->bindParam(":nama", $b);
+        $query->bindParam(":tanggal_lahir", $c);
+
+        if ($query->execute()) {
+            return true;
+        } else {
+            return false;
         }
     }
+
+    public function update($key, $a, $b)
+    {
+        $sql = "UPDATE tb_siswa SET nama=:namabr,tanggal_lahir=:tanggalbr 
+        WHERE nis=:kunci";
+
+        $query = $this->db->prepare($sql);
+
+        $query->bindParam(":kunci", $key);
+        $query->bindParam(":namabr", $a);
+        $query->bindParam(":tanggalbr", $b);
+
+        if ($query->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function delete($key)
+    {
+
+        $sql = "DELETE FROM tb_siswa WHERE nis=:kunci";
+
+        $query = $this->db->prepare($sql);
+
+        $query->bindParam(":kunci", $key);
+
+        if ($query->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 ?>
