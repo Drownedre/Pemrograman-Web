40d466fd708cf82797ce52407249d7fa737acc46
noobie programmer <56153261+Drownedre@users.noreply.github.com>
2023-06-06 06:42:58 +0700

    Update tampil_siswa.php

--- PDO/view/tampil_siswa.php
+++ PDO/view/tampil_siswa.php
@@ -1,50 +1,15 @@
- 
- <?php
-//mengambil folder lain
-include ("../model/model_siswa.php");
 
+<?php
+include "../model/model_siswa.php";
+
+//buat objeks
 $siswa = new mydb();
 $hasil = $siswa->tampilkan();
- ?>
 
- <!DOCTYPE html>
- <html lang="en">
- <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        table, td, th {
-  border: 1px solid;
+foreach ($hasil as $a) {
+    echo $a["id"] . " | " . $a["nis"] .
+        " | " . $a["nama"] . " | " . $a["tanggal_lahir"] .
+        "<a href='update.php?nis=" . $a["nis"] . "'>Update</a>
+        <a href='../controller/proses_delete.php?nis=" . $a["nis"] . "'>Delete</a><br>";
 }
-
-table {
-  width: 100%;
-  border-collapse: collapse;
-}
-    </style>
- </head>
- <body align='center'>
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>NIS</th>
-                <th>NAMA</th>
-                <th>TANGGAL LAHIR</th>
-            </tr>
-        </thead>
-        <?php foreach($hasil as $a){?>
-            <tbody>
-                <tr>
-                    <td><?php echo $a['id'];?></td>
-                    <td><?php echo $a['nis'];?></td>
-                    <td><?php echo $a['nama'];?></td>
-                    <td><?php echo $a['tanggal_lahir'];?></td>             
-                    <?php }; ?>
-                </tr>
-        </tbody>
-    </table>
- </body>
- </html>
+?>
